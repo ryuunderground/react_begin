06629135004435850087e7e9490d0448e2f86705
ryuunderground <jaeking00001@gmail.com>
2024-07-02 16:37:43 +0900

    proptypes, map prac 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import PropTypes from "prop-types";
+
 function Hololive({ name, color }) {
   return <h1 style={{ color: `${color}` }}>I like {name}</h1>;
 }
 
 const idolILike = [
-  { name: "Bae", color: "red" },
-  { name: "Kronii", color: "blue" },
-  { name: "Ame", color: "yellow" },
-  { name: "Gura", color: "aqua" },
+  { name: "Bae", color: "red", id: 0 },
+  { name: "Kronii", color: "blue", id: 1 },
+  { name: "Ame", color: "yellow", id: 2 },
+  { name: "Gura", color: "aqua", id: 3 },
 ];
 
+function IdolDubut(idol) {
+  return <Hololive name={idol.name} color={idol.color} key={idol.id} />;
+}
+
+IdolDubut.propTypes = {
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  key: PropTypes.number.isRequired,
+};
+
 function App() {
-  return (
-    <div className="App">
-      {idolILike.map((idol) => (
-        <Hololive name={idol.name} color={idol.color} />
-      ))}
-    </div>
-  );
+  return <div className="App">{idolILike.map(IdolDubut)}</div>;
 }
 
 export default App;
