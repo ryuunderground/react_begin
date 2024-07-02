a772fd3f0c1272a38195f25d1e8d2f40d3302836
ryuunderground <jaeking00001@gmail.com>
2024-07-02 16:08:55 +0900

    proptype, map practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+function Hololive({ name, color }) {
+  return <h1 style={{ color: `${color}` }}>I like {name}</h1>;
+}
+
+const idolILike = [
+  { name: "Bae", color: "red" },
+  { name: "Kronii", color: "blue" },
+  { name: "Ame", color: "yellow" },
+  { name: "Gura", color: "aqua" },
+];
+
 function App() {
-  return <div className="App">!</div>;
+  return (
+    <div className="App">
+      {idolILike.map((idol) => (
+        <Hololive name={idol.name} color={idol.color} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
