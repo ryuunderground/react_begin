a61783f4e66ceb1793791dce086fc9ba6dac6ab7
ryuunderground <jaeking00001@gmail.com>
2024-07-15 22:09:35 +0900

    theme theory

--- src/App.js
+++ src/App.js
@@ -1,81 +1,24 @@
 import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
-  display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0%;
-  }
-  50%{
-    border-radius: 50%;
-}
-100%{
-  transform: rotate(360deg);
-  border-radius: 00%;
-}
-`;
-
-const Emoiji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  ${Emoiji} {
-    &:hover {
-      font-size: 50px;
-    }
-    &:active {
-      opacity: 0.5;
-    }
-  }
+  align-items: flex-start;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50%;
-`;
-
-const BTN = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
-
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 const App = () => {
   return (
     <>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <div>
-        <BTN>Log in</BTN>
-        <BTN as="a" href="/">
-          Log in
-        </BTN>
-      </div>
-      <Father>
-        <Box bgColor="teal">
-          <Emoiji>😀</Emoiji>
-        </Box>
-        <Circle bgColor="tomato" />
-        <Emoiji>😀</Emoiji>
-      </Father>
+      <Wrapper>
+        <Title>Hello</Title>
+      </Wrapper>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+  textColor: "whiteSmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whiteSmoke",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
