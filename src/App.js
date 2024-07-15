af775a2a7d446cda7c0e5b645f103c90d43c98cb
ryuunderground <jaeking00001@gmail.com>
2024-07-15 18:19:44 +0900

    styled-component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
-import About from "./routes/About";
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
-import Navigation from "./components/Navigation";
+import styled, { keyframes } from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0%;
+  }
+  50%{
+    border-radius: 50%;
+}
+100%{
+  transform: rotate(360deg);
+  border-radius: 00%;
+}
+`;
+
+const Emoiji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoiji} {
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0.5;
+    }
+  }
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
+
+const BTN = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+`;
 
 const App = () => {
   return (
-    <HashRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" exact={true} element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/movie-detail" element={<Detail />} />
-      </Routes>
-    </HashRouter>
+    <>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <div>
+        <BTN>Log in</BTN>
+        <BTN as="a" href="/">
+          Log in
+        </BTN>
+      </div>
+      <Father>
+        <Box bgColor="teal">
+          <Emoiji>😀</Emoiji>
+        </Box>
+        <Circle bgColor="tomato" />
+        <Emoiji>😀</Emoiji>
+      </Father>
+    </>
   );
 };
 
